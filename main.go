6a23ad2d2618fb94be91926f9f96570f9602e770
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-08-17 20:42:01 +0900

    improve: Exit with code 1 if err isn't nil

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/akimon658/pacx/cmd"
+import (
+	"os"
+
+	"github.com/akimon658/pacx/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
